a0241df16053603caf11a4c6140b5ec2d9ab5813
Wallace <wallaceleandro2003@gmail.com>
2025-07-08 21:45:27 -0300

    wpp svg

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,11 +5,12 @@ import Servicos from "../Servicos/Servicos";
 import Sobre from "../Sobre/Sobre";
 import Footer from "../../Components/Footer/Footer";
 import "./Home.css"
+import WhatsAppBtnBottom from "../../Components/WhatsAppBtnBottom/WhatsAppBtnBottom";
 
 function Home() {
     return (
         <>
-
+            <WhatsAppBtnBottom/>
             <Banner />
             <div className="Home">
                 <Servicos />
